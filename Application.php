4df58cb4383c572bf397d02d8d27308475ee8a45
LuK3zz <venhodal.00@spst.eu>
2021-03-03 19:20:23 +0100

    Update

--- Application.php
+++ Application.php
@@ -1,13 +1,11 @@
 <?php
 namespace LuK3zz\score;
 
-use LuK3zz\score\Database\Database;
-
 class Application {
-  public static Database $database;
+  public $db;
 
   public function __construct()
   {
-    self::$database = new Database(env('DB_DSN'), env('DB_USER'), env('DB_PASS'));
+    $this->db = \ParagonIE\EasyDB\Factory::fromArray([env('DB_DSN'), env('DB_USER'), env('DB_PASS')]);
   }
 }
